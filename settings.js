bcdcaee249cf3d2ae942c71c8db5eebf7d8e1dc5
000ganhgsgj <126943919+000ganhgsgj@users.noreply.github.com>
2024-09-27 12:33:22 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "50943212342"
+global.ownername = "Maestro-MD"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
